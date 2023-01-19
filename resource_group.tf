f802254e3eeb0d2c7ee7793ecf090b89610af2ed
Zomb <awsrameshraju@gmail.com>
2023-01-19 18:33:49 +1100

    added task1

--- resource_group.tf
+++ resource_group.tf
@@ -6,4 +6,8 @@ resource "azurerm_resource_group" "rg1" {
   name     = "devopsb27megastar"
   location = "eastus"
 
+  resource "azurerm_resource_group" "task1" {
+  name     = "task1"
+  location = "eastus"
+
 }
