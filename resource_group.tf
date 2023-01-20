3afbd31687630664ff31687c71ff92ce553e20a1
Zomb <awsrameshraju@gmail.com>
2023-01-20 13:06:50 +1100

    added megastar

--- resource_group.tf
+++ resource_group.tf
@@ -6,17 +6,17 @@ resource "azurerm_resource_group" "rg1" {
   name     = "devopsb27megastar"
   location = "eastus"
 
-<<<<<<< HEAD
   resource "azurerm_resource_group" "task1" {
   name     = "task1"
   location = "eastus"
 
-    resource "azurerm_resource_group" "task2" {
+  resource "azurerm_resource_group" "task2" {
   name     = "task2"
-=======
-  resource "azurerm_resource_group" "superstar" {
-  name     = "devopsb27superstar"
->>>>>>> 2492f85c50fcee415375f863f5034141e077ea8b
   location = "eastus"
 
+  resource "azurerm_resource_group" "task3" {
+  name     = "task3"
+  location = "eastus"
+
+  change1 megastar
 }
